9a36b1e5f11544094efeb15280854f8825d5e45b
Jefferson Mira <jefferson.mira@mastercard.com>
2017-05-08 15:47:16 -0500

    Continue trying the login logout

--- src/main/java/com/onechurch/process/config/security/WebSecurityConfig.java
+++ src/main/java/com/onechurch/process/config/security/WebSecurityConfig.java
@@ -7,11 +7,12 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * Created by jeffe on 03/05/2017.
  */
-
+//http://stackoverflow.com/questions/34453693/how-to-authenticate-login-logout-in-restful-web-application-in-spring
 @EnableWebSecurity
 @Configuration
 @ComponentScan("com.onechurch")
@@ -21,14 +22,20 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http
-                .authorizeRequests()
-                .antMatchers("/public/**").permitAll()
-                .anyRequest().authenticated()
+        http.authorizeRequests()
+                    .antMatchers("/public/**").permitAll() // allow any user to access public/
+                    .anyRequest().fullyAuthenticated()  //for any other, it is necessary to authenticate
                 .and()
-                .httpBasic()
+                    .httpBasic()
                 .and()
-                .csrf().disable();
+                    .logout()
+                    .logoutUrl("/logout")
+                    .invalidateHttpSession(true)
+                    .logoutSuccessUrl("/")
+                    .deleteCookies("JSESSIONID")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .and()
+                .csrf();
 
 //        http
 //                .authorizeRequests()
---
+++ src/main/java/com/onechurch/process/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.onechurch.process.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by dc-user on 5/8/2017.
+ */
+
+@RestController
+public class LoginController {
+
+}
